b657b79e7e9fcd0dc79ddaf7007f2075a3707616
Alec Yorgesen <alecyorgesen@gmail.com>
2024-01-30 08:35:40 -0700

    Added a call to the resetBoard in the constuctor

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class ChessBoard {
     private ChessPiece[][] squares = new ChessPiece[9][9];
     public ChessBoard() {
-
+        this.resetBoard();
     }
 
     /**
