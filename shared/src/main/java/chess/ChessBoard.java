afeb22987de7201bd00f19559228235e62b28932
Alec Yorgesen <alecyorgesen@gmail.com>
2024-01-31 08:48:43 -0700

    removed the reset board method from the constructor

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class ChessBoard implements Cloneable {
     private ChessPiece[][] squares = new ChessPiece[9][9];
     public ChessBoard() {
-        this.resetBoard();
+
     }
 
     /**
