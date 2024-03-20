e1c694bf67897b5a570cfdc4586ffd7b1f3c9e29
alecyorgesen <alecyorgesen@gmail.com>
2024-03-19 20:06:42 -0600

    There is now a startup menu!

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -13,27 +13,37 @@ public class Client {
         while (true) {
             System.out.println("Welcome to Chess!!! Please select an option by choosing the appropriate number:");
             System.out.println(" ");
-            System.out.println("1. ");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
+            System.out.println("1. Help.");
+            System.out.println("2. Quit.");
+            System.out.println("3. Login existing user.");
+            System.out.println("4. Register a new user.");
             String line = scanner.nextLine();
             switch (line) {
                 case "1":
-
+                    help();
+                    continue;
+                case "2":
+                    quit();
+                    continue;
+                case "3":
+                    login();
+                    continue;
+                case "4":
+                    register();
+                    continue;
             }
         }
     }
-    public void help() {
-
+    private void help() {
+        System.out.println("SAY BOOO (help)");
     }
-    public void quit() {
-
+    private void quit() {
+        System.out.println("quit");
     }
-    public void login() {
-
+    private void login() {
+        System.out.println("login");
     }
-    public void register() {
-        
+    private void register() {
+        System.out.println("register!");
     }
 }
