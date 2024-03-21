9d9cdeb4e2f14d67daca6aae89e7869350f5f467
alecyorgesen <alecyorgesen@gmail.com>
2024-03-21 00:08:38 -0600

    made a loop that puts the strings for the chessboard in a matrix

--- client/src/main/java/ui/ChessBoardPrinter.java
+++ client/src/main/java/ui/ChessBoardPrinter.java
@@ -1,6 +1,7 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
 
 import java.io.PrintStream;
@@ -9,14 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChessBoardPrinter {
+    final int SQUARE_LENGTH = 3;
+    final int CHESS_BOARD_WIDTH = 10 * SQUARE_LENGTH;
+    final String BLANK_SQUARE = "   ";
     public void printChessBoard(ChessBoard board) {
-        final int SQUARE_LENGTH = 3;
-        final int CHESS_BOARD_WIDTH = 10 * SQUARE_LENGTH;
-        final String BLANK_SQUARE = "   ";
 
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         out.print(EscapeSequences.ERASE_SCREEN);
         out.print(EscapeSequences.SET_BG_COLOR_BLACK);
+
+        String[][] stringMatrix = getPiecePositionsAsStrings(board);
+
+        out.print(BLANK_SQUARE);
+
+        
+    }
+
+    String[][] getPiecePositionsAsStrings(ChessBoard board) {
         String[][] stringMatrix = new String[9][9];
         for (int i = 1; i < 9; i++) {
             for (int j = 1; j < 9; j++) {
@@ -27,23 +37,49 @@ public class ChessBoardPrinter {
                 }
                 switch (chessPiece.getPieceType()) {
                     case PAWN:
-
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " P ";
+                        } else {
+                            stringMatrix[i][j] = " p ";
+                        }
+                        continue;
                     case ROOK:
-
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " R ";
+                        } else {
+                            stringMatrix[i][j] = " r ";
+                        }
+                        continue;
                     case KNIGHT:
-
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " N ";
+                        } else {
+                            stringMatrix[i][j] = " n ";
+                        }
+                        continue;
                     case BISHOP:
-
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " B ";
+                        } else {
+                            stringMatrix[i][j] = " b ";
+                        }
+                        continue;
                     case QUEEN:
-
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " Q ";
+                        } else {
+                            stringMatrix[i][j] = " q ";
+                        }
+                        continue;
                     case KING:
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            stringMatrix[i][j] = " K ";
+                        } else {
+                            stringMatrix[i][j] = " k ";
+                        }
                 }
             }
         }
-
-
-        out.print(BLANK_SQUARE);
-
-        
+        return stringMatrix;
     }
 }
